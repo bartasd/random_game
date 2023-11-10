120fa5d6f5b3594a83b00f7e5a4931a9f7b18314
Bartas Dausynas <bartas3@gmail.com>
2023-11-11 01:33:14 +0200

    meh meh meh

--- js/index.js
+++ js/index.js
@@ -2,11 +2,12 @@ let g_width = window.innerWidth-50;
 let g_height = window.innerHeight-50;
 let box_x = 0;
 let box_y = 0;
+let round = 1;
 
 
 const updateViewport = () => {
     g_width = window.innerWidth-50;
-    g_height = window.innerHeight-50;
+    g_height = window.innerHeight-50-70;
 }
 const randomColor = () => b = '#' + Math.random().toString(16).slice(2,8);
 
@@ -16,21 +17,56 @@ const rand_x_y = () => {
     box_y = Math.floor(Math.random() * g_height);
 }
 
-const debug = () => {
-    console.log("viewport_x: ", window.innerWidth, " box_x", box_x );
-    console.log("viewport_y: ", window.innerHeight, " box_y", box_y );
-    if(box_x > g_width || box_y > g_height)
-        console.log("problemele");
-}
-
 function moveElement() {
     rand_x_y();
-    box.style.top = `${box_x}px`;
-    box.style.left = `${box_y}px`;
+    box.style.top = `${box_y}px`;
+    box.style.left = `${box_x}px`;
     box.style.backgroundColor = randomColor();
-    debug();
     setTimeout(moveElement, 1000);
 }
 
 const box = document.getElementById('box');
-moveElement();
+const abottom = document.getElementById('abottom');
+const login = document.getElementById('login');
+const submit_name = document.getElementById('submit_name');
+const timer = document.getElementById('time');
+
+let name = null;
+function theRoundEnd(){
+    
+}
+
+function start_timer(start) {
+    if(start === 0)
+        theRoundEnd();
+    timer.innerHTML = start--;
+    setTimeout(start_timer(start), 1000);
+}
+
+const start = () => {
+    const roundLength = 30;
+    start_timer(roundLength);
+    //start_round();
+
+}
+
+function getName() {
+    abottom.classList.remove("inputON");
+    box.classList.remove('box_noDisplay');
+    name = document.querySelector('input').value;
+    exitInit();
+    start();
+}
+
+function init(){
+    abottom.classList.add("inputON");
+}
+function exitInit(){
+    login.style.display = "none";
+    document.querySelector(".user > h1").innerHTML = name;
+    moveElement();
+}
+
+submit_name.addEventListener('click', getName);
+
+init();
---
+++ todo.md
@@ -0,0 +1,6 @@
+* brainstorm game logic
+* set limitations for name's input
+* animate user name's input closure
+* fix timer.
+* meh
+* meh meh
