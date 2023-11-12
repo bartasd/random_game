d71517b0aeaeba6d791dd0bf295f48245b552e2e
Bartas Dausynas <bartas3@gmail.com>
2023-11-12 23:52:10 +0200

    mama mia

--- js/index.js
+++ js/index.js
@@ -2,8 +2,9 @@ let g_width = window.innerWidth-50;
 let g_height = window.innerHeight-50;
 let box_x = 0;
 let box_y = 0;
-let round = 1;
-
+let userPoints = 0;
+let userClicked = false;
+let pcPoints = 0;
 
 const updateViewport = () => {
     g_width = window.innerWidth-50;
@@ -22,7 +23,6 @@ function moveElement() {
     box.style.top = `${box_y}px`;
     box.style.left = `${box_x}px`;
     box.style.backgroundColor = randomColor();
-    setTimeout(moveElement, 1000);
 }
 
 const box = document.getElementById('box');
@@ -30,43 +30,64 @@ const abottom = document.getElementById('abottom');
 const login = document.getElementById('login');
 const submit_name = document.getElementById('submit_name');
 const timer = document.getElementById('time');
+const round = document.getElementById('round');
+const userPTS = document.getElementById('userPTS');
+const pcPTS = document.getElementById('pcPTS');
+const playground = document.getElementById('playground');
 
 let name = null;
-function theRoundEnd(){
-    
+
+abottom.classList.add("inputON");
+
+let begin = null;
+let time = 0;
+let timeElapsed = 0
+let roundTime = 30;
+let roundCount = 1;
+
+function start() {
+    userClicked = false;
+    timeElapsed = Date.now() - begin;
+    timer.innerHTML = roundTime;
+    round.innerHTML = roundCount;
+    userPTS.innerHTML = userPoints;
+    pcPTS.innerHTML = pcPoints;
+
+    if(timeElapsed/1000 > time)
+    {
+        if(roundCount === 10 && roundTime === 0)
+            return;
+        time++;
+        roundTime--;
+        moveElement();
+    }
+    if(roundTime === 0)
+    {
+        roundTime = 30;
+        roundCount++;
+    }
+    window.requestAnimationFrame(start);
 }
 
-function start_timer(start) {
-    if(start === 0)
-        theRoundEnd();
-    timer.innerHTML = start--;
-    setTimeout(start_timer(start), 1000);
-}
-
-const start = () => {
-    const roundLength = 30;
-    start_timer(roundLength);
-    //start_round();
-
-}
-
-function getName() {
+function init() {
     abottom.classList.remove("inputON");
     box.classList.remove('box_noDisplay');
     name = document.querySelector('input').value;
-    exitInit();
+    login.style.display = "none";
+    document.querySelector(".user > h1").innerHTML = name;
+    begin = Date.now();
     start();
 }
 
-function init(){
-    abottom.classList.add("inputON");
+const userBONUS = () => {
+    userPoints++;
+    userClicked = true;
 }
-function exitInit(){
-    login.style.display = "none";
-    document.querySelector(".user > h1").innerHTML = name;
-    moveElement();
+const pcBONUS = () => {
+    pcPoints++;
+    userClicked = true;
 }
 
-submit_name.addEventListener('click', getName);
-
-init();
+submit_name.addEventListener('click', init);
+box.addEventListener('click', userBONUS);
+playground.addEventListener('click', pcBONUS);
